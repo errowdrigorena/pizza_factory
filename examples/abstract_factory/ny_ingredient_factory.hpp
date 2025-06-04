de01e613363cafca1f1b2de771e2c96783765975
errowdrigorena <xxlgran@gmail.com>
2025-06-04 09:29:12 +0200

    fix: corrige includes y estilo en ny_ingredient_factory.hpp para compatibilidad y portabilidad

--- examples/abstract_factory/ny_ingredient_factory.hpp
+++ examples/abstract_factory/ny_ingredient_factory.hpp
@@ -1,16 +1,20 @@
 #pragma once
-#include "pizza_ingredient_factory.hpp"
-#include "ingredients/concrete_ingredients.hpp"
-#include "factory/concrete_creator.hpp"
+
+#include <memory>
+#include <string>
+
+#include <pizza_ingredient_factory.hpp>
+#include <ingredients/concrete_ingredients.hpp>
+#include <factory/concrete_creator.hpp>
 
 class NyIngredientFactory final : public PizzaIngredientFactory {
 public:
     std::unique_ptr<Dough> create_dough() const override {
-        return dough_creator_().create();
+        return dough_creator_.create();
     }
 
     std::unique_ptr<Topping> create_topping() const override {
-        return topping_creator_().create();
+        return topping_creator_.create();
     }
 
 private:
