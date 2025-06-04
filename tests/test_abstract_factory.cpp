8e0bf535ad2ad2a3be78e0d136adb2ca0c7d8153
errowdrigorena <xxlgran@gmail.com>
2025-06-04 09:29:02 +0200

    test: usa includes limpios <abstract_factory/...> en test_abstract_factory.cpp

--- tests/test_abstract_factory.cpp
+++ tests/test_abstract_factory.cpp
@@ -1,8 +1,15 @@
-#include "chicago_ingredient_factory.hpp"
+#include <abstract_factory/chicago_ingredient_factory.hpp>
+#include <abstract_factory/ny_ingredient_factory.hpp>
 #include <gtest/gtest.h>
 
 TEST(ChicagoIngredientFactoryTest, ProducesCorrectDescriptions) {
     ChicagoIngredientFactory factory;
     EXPECT_EQ(factory.create_dough()->description(), "Thick Crust Dough");
     EXPECT_EQ(factory.create_topping()->description(), "Spicy Pepperoni");
+}
+
+TEST(NewYorkIngredientFactoryTest, ProducesCorrectDescriptions) {
+    NyIngredientFactory factory;
+    EXPECT_EQ(factory.create_dough()->description(), "Thin Crust Dough");
+    EXPECT_EQ(factory.create_topping()->description(), "Crispy Bacon");
 }
